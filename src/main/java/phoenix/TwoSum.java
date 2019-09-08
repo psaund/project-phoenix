4ba33dc73b6d558d8aa3f877e46c1a724d98466b
psaund <prashant.saund@gmail.com>
2019-09-08 20:47:11 +0000

    TwoSum

---
+++ src/main/java/phoenix/TwoSum.java
@@ -0,0 +1,24 @@
+package phoenix;
+
+public class TwoSum {
+    
+    public static int[] nums = {2, 7, 11, 15};
+    
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> complementMap = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int complement = target - nums[i];
+            if(complementMap.containsKey(complement)){
+                return new int[]{i, complementMap.get(complement)};
+            }else {
+                complementMap.put(nums[i], i);
+            }
+            
+        }
+        
+    }
+    
+    public static void main(String... argvs) {
+        System.out.println("TwoSum solution: " + twoSum(nums, 13));
+    }
+}
