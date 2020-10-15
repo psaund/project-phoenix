43634f60413c32415013368c0eed21c335ccbad4
pcs001d <prashant.saundade@walmartlabs.com>
2020-10-14 21:49:31 -0700

    update

--- src/main/java/phoenix/AddTwoNumbers2.java
+++ src/main/java/phoenix/AddTwoNumbers2.java
@@ -2,6 +2,25 @@ package phoenix;
 
 public class AddTwoNumbers2 {
 
+	public static ListNode addTwoNumbersTry(ListNode l1, ListNode l2) {
+	    ListNode dummyHead = new ListNode(0);
+	    ListNode p = l1, q = l2, curr = dummyHead;
+	    int carry = 0;
+	    while (p != null || q != null) {
+	        int x = (p != null) ? p.val : 0;
+	        int y = (q != null) ? q.val : 0;
+	        int sum = carry + x + y;
+	        carry = sum / 10;
+	        curr.next = new ListNode(sum % 10);
+	        curr = curr.next;
+	        if (p != null) p = p.next;
+	        if (q != null) q = q.next;
+	    }
+	    if (carry > 0) {
+	        curr.next = new ListNode(carry);
+	    }
+	    return dummyHead.next;
+	}
 	
 	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 	    ListNode dummyHead = new ListNode(0);
@@ -25,7 +44,7 @@ public class AddTwoNumbers2 {
 	
 	public static void main(String[] args) {
 		//represents number 342
-		ListNode l1 = new ListNode(7, new ListNode(4, new ListNode(6))); 
+		ListNode l1 = new ListNode(3, new ListNode(4, new ListNode(2))); 
 		ListNode l2 = new ListNode(4, new ListNode(6, new ListNode(5)));
 		print(l1);
 		print(l2);
@@ -44,10 +63,10 @@ public class AddTwoNumbers2 {
 	}
 	
 }
-class ListNode {
+/*class ListNode {
 	int val;
 	ListNode next;
 	ListNode() {}
 	ListNode(int val) { this.val = val; }
 	ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
+}*/
